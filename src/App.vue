9854bcb47a4ccb30aa90189f23c76547c1b12ad1
ricardospalves <ricardospalves@gmail.com>
2019-10-23 15:55:26 -0300

    Usando rotas nomeadas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
   <div id="app">
+    <ul>
+      <li>
+        <router-link
+          :to="{
+            name: 'home'
+          }"
+        >
+          Home
+        </router-link>
+      </li>
+      <li
+        v-for="team in teams"
+        :key="team.id"
+      >
+        <router-link
+          :to="{
+            name: 'team',
+            params: {
+              id: team.id
+            }
+          }"
+        >
+          {{ team.shortname }}
+        </router-link>
+      </li>
+    </ul>
     <router-view/>
   </div>
 </template>
