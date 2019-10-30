3921c640c79355ff6616fdc90d238ce7e3a76796
ricardospalves <ricardospalves@gmail.com>
2019-10-30 18:06:54 -0300

    Método computado para pegar os últimos campeões

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,7 +41,55 @@ export default {
   computed: {
     ...mapGetters([
       'randomTeams'
-    ])
+    ]),
+    mostRecentChampions() {
+      const mostRecent = key => {
+        return this.randomTeams
+          .filter(team => team.honours[key].length)
+          .reduce((accumulator, current) => {
+            const accumulatorMostRecent = Math.max(...accumulator.honours[key])
+            const currentMostRecent = Math.max(...current.honours[key])
+            const boolean = accumulatorMostRecent > currentMostRecent
+
+            return boolean ? accumulator : current
+          })
+      }
+
+      return [
+        {
+          competition: 'Mundial',
+          team: mostRecent('mundial')
+        },
+        {
+          competition: 'Libertadores',
+          team: mostRecent('libertadores')
+        },
+        {
+          competition: 'Sul-Americana',
+          team: mostRecent('sulAmericana')
+        },
+        {
+          competition: 'Recopa',
+          team: mostRecent('recopa')
+        },
+        {
+          competition: 'J.League/CONMEBOL',
+          team: mostRecent('jLeagueConmebol')
+        },
+        {
+          competition: 'Brasileirão',
+          team: mostRecent('brasileirao')
+        },
+        {
+          competition: 'Copa do Brasil',
+          team: mostRecent('copaDoBrasil')
+        },
+        {
+          competition: 'Copa dos Campeões',
+          team: mostRecent('copaDosCampeoes')
+        }
+      ]
+    }
   }
 }
 </script>
