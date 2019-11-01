8dfdfddb0fe6edbe6138a5e328c8947ad5d92ccb
ricardospalves <ricardospalves@gmail.com>
2019-11-01 13:45:25 -0300

    Removendo o método que pega os campeões mais recentes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,5 @@
 <template>
   <div>
-    <div
-      v-for="champion in latestChampions"
-      :key="champion.competition"
-    >
-      <h2>
-        {{ champion.competition }}
-      </h2>
-
-      <p>
-        {{ champion.team.name }}
-      </p>
-    </div>
-
     <ul>
       <li
         v-for="team in randomTeams"
@@ -53,45 +40,8 @@ export default {
   },
   computed: {
     ...mapGetters([
-      'randomTeams',
-      'latestCompetitionChampion'
-    ]),
-    latestChampions() {
-      return [
-        {
-          competition: 'Mundial',
-          team: this.latestCompetitionChampion('mundial')
-        },
-        {
-          competition: 'Libertadores',
-          team: this.latestCompetitionChampion('libertadores')
-        },
-        {
-          competition: 'Sul-Americana',
-          team: this.latestCompetitionChampion('sulAmericana')
-        },
-        {
-          competition: 'Recopa',
-          team: this.latestCompetitionChampion('recopa')
-        },
-        {
-          competition: 'J.League/CONMEBOL',
-          team: this.latestCompetitionChampion('jLeagueConmebol')
-        },
-        {
-          competition: 'Brasileirão',
-          team: this.latestCompetitionChampion('brasileirao')
-        },
-        {
-          competition: 'Copa do Brasil',
-          team: this.latestCompetitionChampion('copaDoBrasil')
-        },
-        {
-          competition: 'Copa dos Campeões',
-          team: this.latestCompetitionChampion('copaDosCampeoes')
-        }
-      ]
-    }
+      'randomTeams'
+    ])
   }
 }
 </script>
