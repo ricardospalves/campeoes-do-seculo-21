c6ed204c7c2258106aca7dcd6929779f0a3f58e0
ricardospalves <ricardospalves@gmail.com>
2019-12-19 11:55:36 -0300

    Adicionar estilos para o Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,17 @@
 <template>
-  <header>
-    <h1>
-      <router-link
-        :to="{
-          name: 'home'
-        }"
-      >
-        {{ appName }}
-      </router-link>
-    </h1>
+  <header class="px-2 border-b border-solid border-gray-300">
+    <div class="flex max-w-5xl mx-auto h-12">
+      <h1 class="text-lg font-bold uppercase">
+        <router-link
+          class="flex items-center h-full"
+          :to="{
+            name: 'home'
+          }"
+        >
+          {{ appName }}
+        </router-link>
+      </h1>
+    </div>
   </header>
 </template>
 
