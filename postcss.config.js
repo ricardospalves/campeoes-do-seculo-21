53b9a7b60c5eedb0677998612b068270b0ccafbf
Ricardo Alves <ricardospalves@gmail.com>
2019-12-24 18:58:14 -0300

    Corrigir as configurações do PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,18 @@
+const tailwindcss = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
   'plugins': [
-    require('tailwindcss')('tailwind.js'),
+    tailwindcss('tailwind.js'),
     require('autoprefixer')(),
     //Only add purgecss in production
-    process.env.NODE_ENV === 'production'? require('@fullhuman/postcss-purgecss')({
-        content: [
-            './src/**/*.html',
-            './src/**/*.vue'
-        ]
-    }): ''
+    process.env.NODE_ENV === 'production' ? purgecss({
+      content: [
+        './src/**/*.vue',
+        './src/**/*.js'
+      ],
+      // Include any special characters you're using in this regular expression
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    }) : ''
   ]
 }
