3529c135f1368c45ff6c9cbd6ccc60ab294e1354
Ricardo Alves <ricardospalves@gmail.com>
2019-12-28 22:59:19 -0300

    Adicionar logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,21 @@
 <template>
   <header class="px-2 border-b border-solid border-gray-300">
-    <div class="flex justify-between max-w-5xl mx-auto h-12">
+    <div class="flex justify-between max-w-5xl mx-auto h-16">
       <h1 class="text-lg font-bold uppercase">
         <router-link
-          class="flex items-center h-full"
+          class="flex items-center w-16 h-full"
+          :title="appName"
           :to="{
             name: 'home'
           }"
         >
-          {{ appName }}
+          <svg class="block w-full h-full fill-current">
+            <use xlink:href="#icon-logo"/>
+          </svg>
+
+          <span class="sr-only">
+            {{ appName }}
+          </span>
         </router-link>
       </h1>
 
