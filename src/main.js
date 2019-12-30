0ef4a3643c08b225746930886bd5414b56ced644
Ricardo Alves <ricardospalves@gmail.com>
2019-12-30 20:49:06 -0300

    Adicionar PWA

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 
 import '@/assets/css/tailwind.css'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pwa: {
+    name: 'Campeões do Século 21',
+    themeColor: '#FFFFFF',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    manifestOptions: {
+      start_url: '/'
+    }
+  }
+}
